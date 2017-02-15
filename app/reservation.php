55e443a2e141e42a8ed23e84cac9408baa8c976e
ojoven <mikeltorresmail@gmail.com>
2017-02-15 13:25:39 +0100

    Reservation connect front end and backend

--- app/reservation.php
+++ app/reservation.php
@@ -16,29 +16,36 @@ $client = SesClient::factory(array(
 // We get the params
 $params = $_POST;
 if (!isset($params['amount']) || !isset($params['name']) || !isset($params['email'])) { // Wooo, such validation!
-	die('Nope');
+	$response['success'] = false;
+} else {
+
+	// SEND EMAIL
+	$msg = array();
+	$msg['Source'] = $email; // Yes, we're sending emails to ourselves
+	$msg['Destination']['ToAddresses'][] = $email;
+
+	$msg['Message']['Subject']['Data'] = "[Mêléeton 2017] Reservar " . $params['amount'] . " plaza(s) para " . $params['name'] . " - " . $params['email'];
+	$msg['Message']['Subject']['Charset'] = "UTF-8";
+
+	$msg['Message']['Body']['Text']['Data'] = "[Mêléeton 2017] Reservar " . $params['amount'] . " plaza(s) para " . $params['name'] . " - " . $params['email'];
+	$msg['Message']['Body']['Text']['Charset'] = "UTF-8";
+	$msg['Message']['Body']['Html']['Data'] = "[Mêléeton 2017] Reservar " . $params['amount'] . " plaza(s) para " . $params['name'] . " - " . $params['email'];
+	$msg['Message']['Body']['Html']['Charset'] = "UTF-8";
+
+	$result = array();
+
+	try{
+		$result = $client->sendEmail($msg);
+		$response['success'] = true;
+	} catch (Exception $e) {
+		//An error happened and the email did not get sent
+		$response['success'] = false;
+		$response['message'] = $e->getMessage();
+	}
+
+
 }
 
-// SEND EMAIL
-$msg = array();
-$msg['Source'] = $email; // Yes, we're sending emails to ourselves
-$msg['Destination']['ToAddresses'][] = $email;
-
-$msg['Message']['Subject']['Data'] = "[Mêléeton 2017] Reservar " . $params['amount'] . " plaza(s) para " . $params['name'] . " - " . $params['email'];
-$msg['Message']['Subject']['Charset'] = "UTF-8";
-
-$msg['Message']['Body']['Text']['Data'] = "[Mêléeton 2017] Reservar " . $params['amount'] . " plaza(s) para " . $params['name'] . " - " . $params['email'];
-$msg['Message']['Body']['Text']['Charset'] = "UTF-8";
-$msg['Message']['Body']['Html']['Data'] = "[Mêléeton 2017] Reservar " . $params['amount'] . " plaza(s) para " . $params['name'] . " - " . $params['email'];
-$msg['Message']['Body']['Html']['Charset'] = "UTF-8";
-
-try{
-	$result = $client->sendEmail($msg);
-
-	//save the MessageId which can be used to track the request
-	$msg_id = $result->get('MessageId');
-	echo("MessageId: $msg_id");
-} catch (Exception $e) {
-	//An error happened and the email did not get sent
-	echo($e->getMessage());
-}
+header('Content-Type: application/json');
+echo json_encode($response);
+die();
--- js/src/app.js
+++ js/src/app.js
@@ -88,6 +88,37 @@
 
     });
 
+    $('#to-reservation').on('click', function() {
+
+        var url = '/app/reservation.php';
+        var data = {};
+        data.name = $('#name').val();
+        data.email = $('#email').val();
+        data.amount = $('#amount').val();
+
+        var $firstStep = $('.buy-ticket-info .first-step');
+        var $loading = $('.buy-ticket-info .loading');
+        var $lastStep = $('.buy-ticket-info .last-step');
+
+        // Go to step 2 (loading)
+        $firstStep.hide();
+        $loading.show();
+
+        $.post(url, data, function(response) {
+
+            // Go to step 3 (response)
+            $loading.hide();
+            $lastStep.show();
+
+            if (response.success) {
+                $lastStep.addClass('success').html('¡Genial! Hemos recibido tu reserva,<br>nos pondremos en contacto contigo en los próximos días.');
+            } else {
+                $lastStep.addClass('error').html('Parece que hubo algún error.<br>Por favor, contacta con <a href="http://twitter.com/ojoven" target="_blank">http://twitter.com/ojoven</a>');
+            }
+
+        });
+    });
+
     // HYPE GENERATOR
     var hype = 0;
     var $hypeButton = $(".hype-button");
